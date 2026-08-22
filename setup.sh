681b4c99781ce797cc933b9515a0cc9e9a97b3e8
Dayvinksthik <daylightgvguv@gmail.com>
2026-08-22 19:07:32 +0800

    Update installation message and Discord link

--- setup.sh
+++ setup.sh
@@ -30,4 +30,4 @@ mkdir -p /storage/emulated/0/Android/data/com.roblox.client/files/delta/autoexec
 mkdir -p /storage/emulated/0/Android/data/com.roblox.client/files/fluxus/autoexec
 mkdir -p /storage/emulated/0/RobloxClone001/Codex/Autoexec/
 
-echo "Installation complete! -- Koala Collective | https://discord.gg/KoalaHub"
+echo "Installation complete! -- Nexora Collective | https://discord.gg/dCMVzpBuTm"
